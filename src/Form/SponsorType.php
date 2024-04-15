028e2232e653e2533df9bbd870618d39f0e0c36d
Alilovez <160535202+Alilovez@users.noreply.github.com>
2024-04-15 21:48:02 +0100

    update

---
+++ src/Form/SponsorType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Sponsor;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class SponsorType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('email')
+            ->add('numtel', IntegerType::class
+            )
+            ->add('status', ChoiceType::class, [
+                'choices' => [
+                    '' => '',
+                    'Accepted' => 'accepted',
+                    'Rejected' => 'rejected',
+                ],
+                'data' => '', // Set initial value to '
+               
+                'required' => true,
+            ])
+            ->add('budget', IntegerType::class, [
+                'data' => 0, // Set initial value to 0
+            ])
+            ->add('cause', TextType::class, [
+                'data' => '', // Set initial value to an empty string
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Sponsor::class,
+        ]);
+    }
+}
